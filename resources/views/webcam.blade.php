4db521c16aace8efd8caf550e2ff67a9d6818e1f
towhidappaloy <towhid@appaloy.com>
2022-07-25 18:10:31 +0600

    Dropify Preview

--- resources/views/webcam.blade.php
+++ resources/views/webcam.blade.php
@@ -6,10 +6,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css" integrity="sha512-EZSUkJWTjzDlspOoPSpUFR0o0Xy7jdzW//6qhUkoZ9c4StFkVsp9fbbd0O06p9ELS3H486m4wmrCELjza4JEog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="{{asset('custom.js')}}"></script>
 
     <style type="text/css">
         #results { padding:20px; border:1px solid; background:#ccc; }
     </style>
+    
 </head>
 <body>
      
@@ -23,13 +25,11 @@
                 <div id="my_camera"></div>
                 <br/>
                 <input type=button value="Take Snapshot" onClick="take_snapshot()">
-                <input type="hidden" name="image" class="image-tag">
+                <input type="hidden" name="File" class="dropify" data-default-file="">
             </div>
             <div class="col-md-6">
                 <div id="results">Your captured image will appear here...</div>
             </div>
-
-            <input type="file" class="form-control" name="avatar" id="avatar" />
             <div class="col-md-12 text-center">
                 <br/>
                 <button class="btn btn-success">Submit</button>
@@ -52,14 +52,27 @@
      
     function take_snapshot() {
         Webcam.snap( function(data_uri) {
-            $(".image-tag").val(data_uri);
-            document.getElementById('results').innerHTML = '<img src="'+data_uri+'"/>';
-            $("#avatar").addClass('dropify');
-            $("#avatar").attr("data-default-file",data_uri);
             $('.dropify').dropify();
-            console.log(data_uri);
-        } );
+            resetPreview('File', data_uri,'Image.jpg');
+        });
+
+        function resetPreview(name, src, fname = '') {
+            let input = $('input[name="' + name + '"]');
+            let wrapper = input.closest('.dropify-wrapper');
+            let preview = wrapper.find('.dropify-preview');
+            let filename = wrapper.find('.dropify-filename-inner');
+            let render = wrapper.find('.dropify-render').html('');
+
+            input.val('').attr('title', fname);
+            wrapper.removeClass('has-error').addClass('has-preview');
+            filename.html(fname);
+
+            render.append($('<img />').attr('src', src).css('max-height', input.data('height') || ''));
+            preview.fadeIn();
+            }
     }
+
+    
     
 </script>
     
